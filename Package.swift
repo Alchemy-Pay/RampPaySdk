51e3aeb27da969fe50c2c6f4446e99350ff735eb
{} <houyu@1199549734223602.onaliyun.com>
2024-09-14 14:46:36 +0800

    pay

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "RampPaySdk",
     platforms: [
-        .iOS(.v14) // 设置你支持的最低 iOS 版本
+        .iOS(.v11) // 设置你支持的最低 iOS 版本
     ],
     products: [
         .library(
@@ -11,10 +12,13 @@ let package = Package(
             targets: ["RampPaySdk"]
         ),
     ],
+    dependencies: [
+        // 在此添加依赖项，如果有的话
+    ],
     targets: [
         .binaryTarget(
             name: "RampPaySdk",
-            path: "./RampPaySdk.xcframework"
+             path: "./RampPaySdk.xcframework"
         )
     ]
 )
